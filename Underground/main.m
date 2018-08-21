a39f1c0b48d60856cd515b791cd5792cd790691e
panxiaorui <panxiaorui@meituan.com>
2018-08-21 13:50:55 +0800

    Initial Commit

---
+++ Underground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Underground
+//
+//  Created by 潘潇睿 on 2018/8/21.
+//  Copyright © 2018年 潘潇睿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
